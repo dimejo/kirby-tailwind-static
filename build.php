3f3941cb0f6778cfb05d463212eb14153b227e1b
Alex JOST <jost+github@dimejo.at>
2024-02-11 15:59:26 +0100

    'kirby3-static-site-generator' added

---
+++ build.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . '/kirby/bootstrap.php';
+require __DIR__ . '/site/plugins/static-site-generator/index.php';
+
+$kirby = kirby();
+$site = $kirby->site();
+
+$staticSiteGenerator = new D4L\StaticSiteGenerator($kirby, $pathsToCopy = [$kirby->roots()->assets(), $kirby->roots()->media()], $pages = null);
+$fileList = $staticSiteGenerator->generate($outputFolder = './static', $baseUrl = 'https://cdn.dimejo.at/', $preserve = []);
