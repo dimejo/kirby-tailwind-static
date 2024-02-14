b55aff810250456070514c799ff471b1cf6fcb34
Alex JOST <jost+github@dimejo.at>
2024-02-14 16:44:09 +0100

    staticSiteGenerator: Remove $pathsToCopy
    Setting `$pathsToCopy` is necessary when the website is build by calling `build.php` in the browser. Setting `$pathsToCopy` is not necessary when the website is build by running `php build.php` on the command line.

--- build.php
+++ build.php
@@ -6,5 +6,5 @@ require __DIR__ . '/site/plugins/static-site-generator/index.php';
 $kirby = kirby();
 $site = $kirby->site();
 
-$staticSiteGenerator = new D4L\StaticSiteGenerator($kirby, $pathsToCopy = [$kirby->roots()->assets(), $kirby->roots()->media()], $pages = null);
+$staticSiteGenerator = new D4L\StaticSiteGenerator($kirby, $pathsToCopy = null, $pages = null);
 $fileList = $staticSiteGenerator->generate($outputFolder = './static', $baseUrl = '/static', $preserve = []);
