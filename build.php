d8778180acd42dc8bd375263ff76224e5f068fa3
Alex JOST <jost+github@dimejo.at>
2024-02-14 16:32:52 +0100

    build.php: Adjust baseUrl for Github Pages

--- build.php
+++ build.php
@@ -7,4 +7,4 @@ $kirby = kirby();
 $site = $kirby->site();
 
 $staticSiteGenerator = new D4L\StaticSiteGenerator($kirby, $pathsToCopy = [$kirby->roots()->assets(), $kirby->roots()->media()], $pages = null);
-$fileList = $staticSiteGenerator->generate($outputFolder = './static', $baseUrl = '/', $preserve = []);
+$fileList = $staticSiteGenerator->generate($outputFolder = './static', $baseUrl = '/static', $preserve = []);
